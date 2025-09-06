373a51bb2b1500d32e3588eff54dd3ae6415c1cb
z0xca <nextdnnds@proton.me>
2025-09-06 09:48:51 -0400

    add callout and lint

--- astro.config.ts
+++ astro.config.ts
@@ -3,15 +3,19 @@
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
+import remarkCallout from "@r4ai/remark-callout";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false
-	},		
+	devToolbar: {
+		enabled: false,
+	},
 	vite: {
 		plugins: [tailwindcss()],
 	},
 	integrations: [react(), mdx()],
+	markdown: {
+		remarkPlugins: [remarkCallout],
+	},
 });
