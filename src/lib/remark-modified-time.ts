61d4e4e4a700d1931d6085285dad8a9cf2ea4291
z0xca <185145122+z0xca@users.noreply.github.com>
2026-01-04 17:59:10 -0500

    point modification date to commit

--- src/lib/remark-modified-time.ts
+++ src/lib/remark-modified-time.ts
@@ -3,7 +3,12 @@ import { execSync } from "child_process";
 export function remarkModifiedTime() {
   return function (tree, file) {
     const filepath = file.history[0];
-    const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
-    file.data.astro.frontmatter.lastModified = result.toString();
+    const result = execSync(`git log -1 --pretty="format:%cI %H" "${filepath}"`);
+    const output = result.toString().trim();
+    if (output) {
+      const [date, commit] = output.split(" ");
+      file.data.astro.frontmatter.lastModified = date;
+      file.data.astro.frontmatter.lastModifiedCommit = commit;
+    }
   };
 }
